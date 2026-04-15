94d4cbc1e1763dff410c181dfb956d822e994f66
staslolpol <staspriho9@gmail.com>
2026-04-15 17:20:36 +0300

    Task 01 comlite

--- Task01/main.cpp
+++ Task01/main.cpp
@@ -1,15 +1,18 @@
-#include "util.h"
+#include "logic.h"
 
 int main() {
 	int matrix[DEFAULT_SIZE][DEFAULT_SIZE];
 	int n, m;
 
-	cout << "Input size of matrix(n, m): ";
+	cout << "Input square of matrix(n, m): ";
 	cin >> n >> m;
 
 	init(matrix, n, m, 0, 10);
 
-	cout << "Matrix: " << endl << convert(matrix, n, m) << endl;
+	cout << "Matrix: " << endl << convert(matrix, n, n) << endl;
+
+	int sum = sum_main_elements(matrix, n);
+	cout << "Sum of main diagonal elements if: " << sum;
 
 	return 0;
 }
